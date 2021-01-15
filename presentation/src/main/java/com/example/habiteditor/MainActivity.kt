a211093699aedba01d6ae2cdb3d17dc6f6849478
HerbalGreenTea <alex.andros.solovev@gmail.com>
2021-01-15 11:07:23 +0500

    добавлен Navigation component

--- presentation/src/main/java/com/example/habiteditor/MainActivity.kt
+++ presentation/src/main/java/com/example/habiteditor/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.habiteditor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
